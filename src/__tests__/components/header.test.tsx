fa4be86aa8d0ccae2a051d0b0860254d07415097
Akvaro03 <alvaroballarini2010@gmail.com>
2024-10-08 14:58:45 -0300

    feature:end header firsts test

--- src/__tests__/components/header.test.tsx
+++ src/__tests__/components/header.test.tsx
@@ -1,9 +1,56 @@
 import HeaderComponent from "@/components/HeaderComponent";
 import { act, render, screen } from "@testing-library/react";
+import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
 
-it("render header Component", async () => {
+jest.mock("react-firebase-hooks/auth", () => ({
+  useAuthState: jest.fn(),
+  useSignOut: jest.fn(),
+}));
+
+describe("Header Component", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("render header tittles", async () => {
+    (useAuthState as jest.Mock).mockReturnValue([null, false]);
+
+    // Simula que useSignOut devuelve una función mockeada para cerrar sesión
+    (useSignOut as jest.Mock).mockReturnValue([jest.fn()]);
     await act(async () => {
-        render(<HeaderComponent />);
-      });
-      expect(screen.getByText("Explore")).toBeInTheDocument();
+      render(<HeaderComponent />);
+    });
+
+    expect(screen.getByText(/Dev/i)).toBeInTheDocument();
+    expect(screen.getByText(/Forges/i)).toBeInTheDocument();
+    expect(screen.getByText(/Explore/i)).toBeInTheDocument();
+    expect(screen.getByText(/Saves/i)).toBeInTheDocument();
+  });
+
+  it("render header when user is not loaded", async () => {
+    (useAuthState as jest.Mock).mockReturnValue([null, false]);
+
+    // Simula que useSignOut devuelve una función mockeada para cerrar sesión
+    (useSignOut as jest.Mock).mockReturnValue([jest.fn()]);
+
+    await act(async () => {
+      render(<HeaderComponent />);
+    });
+
+    expect(screen.getByText(/Create Account/i)).toBeInTheDocument();
+    expect(screen.getByText(/Sing in/i)).toBeInTheDocument();
+  });
+  it("render header when user is loaded", async () => {
+    const user = { uid: "123", displayName: "Test User" };
+
+    (useAuthState as jest.Mock).mockReturnValue([user, false]);
+
+    // Simula que useSignOut devuelve una función mockeada para cerrar sesión
+    (useSignOut as jest.Mock).mockReturnValue([jest.fn()]);
+
+    await act(async () => {
+      render(<HeaderComponent />);
+    });
+    expect(screen.getByText(/Log Out/i)).toBeInTheDocument();
+  });
 });
