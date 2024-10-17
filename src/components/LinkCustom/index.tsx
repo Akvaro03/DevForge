1e162a002c8fa11555e309bc66891588624cb69f
Akvaro03 <alvaroballarini2010@gmail.com>
2024-10-17 13:36:50 -0300

    fix: fixed styles

--- src/components/LinkCustom/index.tsx
+++ src/components/LinkCustom/index.tsx
@@ -1,15 +1,31 @@
 import Link from "next/link";
 import { ReactElement } from "react";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 // function LinkCustom({ url, tittle, icon, onClick }: { url: string, tittle: string, icon?: ReactElement, onClick?: () => void }) {
-function LinkCustom({ url, children }: { url: string, children: ReactElement }) {
-    const pathName = usePathname()
-    return (
-        <Link style={{ color: url === pathName ? "var(--first-theme)" : "white" }} href={url}>
-            {children}
-        </Link>
-    )
+function LinkCustom({
+  url,
+  children,
+}: {
+  url: string;
+  children: ReactElement;
+}) {
+  const pathName = usePathname();
+  return (
+    <Link
+      style={
+        url === pathName
+          ? {
+              color: "var(--first-theme)",
+              background: "linear-gradient(135deg, #1d1f21, #333)",
+              borderRadius: "10px",
+            }
+          : undefined
+      }
+      href={url}
+    >
+      {children}
+    </Link>
+  );
 }
-
 export default LinkCustom;
